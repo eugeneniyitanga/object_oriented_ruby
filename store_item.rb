4ed50e6466e4bc1fbb989814d2618bb1555d69db
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-01-11 14:23:04 +0200

    added three new items available in the store

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1= {"product_name" => "samsung", "color" => "white", "price" => 10000}
+item2= {"product_name" => "tecno_J8", "color" => "black", "price" => 60000}
+item3= {"product_name" => "HTC_grand", "color" => "pink", "price" => 80000} 
