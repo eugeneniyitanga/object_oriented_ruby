8e75682cd51edcb61ad0e8ab8671953cfe233133
Eugene Niyitanga <eugene.niyitanga2015@gmail.com>
2017-01-11 14:50:33 +0200

    added what it should display

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 item1= {"product_name" => "samsung", "color" => "white", "price" => 10000}
 item2= {"product_name" => "tecno_J8", "color" => "black", "price" => 60000}
 item3= {"product_name" => "HTC_grand", "color" => "pink", "price" => 80000} 
+
+puts "The available product is #{item3["product_name"]} and its price is #{item3["price"]}"
